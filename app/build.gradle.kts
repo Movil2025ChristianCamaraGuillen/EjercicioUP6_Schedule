8be8552842c79b8c81e097488f50f0b3d217cece
Christian Cámara <chcamaraguillen@gmail.com>
2026-04-28 20:59:43 +0200

    Punto 4 realizado, crear una entidad de room + dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,10 @@ android {
 
 dependencies {
 
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+
     implementation(platform("androidx.compose:compose-bom:2024.11.00"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
